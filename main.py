f1e3ef6b99e72359e842b9b5f151d792de6071c0
zakiell1209 <gonsiksmerti@gmail.com>
2025-06-15 14:06:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import logging
-from aiogram import Bot, Dispatcher, executor, types
+import os
+from fastapi import FastAPI, Request
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Update
+from bot.handlers import router
+from dotenv import load_dotenv
 
-API_TOKEN = "ВАШ_ТОКЕН_ТЕЛЕГРАМ"
+load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
+dp.include_router(router)
 
-@dp.message_handler(commands=["start", "help"])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я бот на aiogram 2.x, работаю без Rust.")
+app = FastAPI()
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@app.on_event("startup")
+async def on_startup():
+    await bot.set_webhook(f"{WEBHOOK_URL}")
 
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+@app.post("/webhook")
+async def telegram_webhook(req: Request):
+    body = await req.json()
+    update = Update.model_validate(body)
+    await dp.feed_update(bot, update)
+    return {"status": "ok"}
